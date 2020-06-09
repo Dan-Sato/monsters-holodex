511f97859c8f44ee6b3851a976d7a1ebc6872be5
dan-sato <danharuosato@gmail.com>
2020-06-09 11:41:08 -0300

    Updates

---
+++ precache-manifest.2c2cbcd33eade3db550ef453f5c565b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28791ac49066bcaf77e29a452fdd3e49",
+    "url": "/monsters-holodex/index.html"
+  },
+  {
+    "revision": "661630b33124f37363a8",
+    "url": "/monsters-holodex/static/css/main.bc55ee0d.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-holodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-holodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661630b33124f37363a8",
+    "url": "/monsters-holodex/static/js/main.84c3c8a5.chunk.js"
+  },
+  {
+    "revision": "fb471c3a224829153520",
+    "url": "/monsters-holodex/static/js/runtime-main.2f34471e.js"
+  }
+]);
